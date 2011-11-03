587255eea53c33723ed0494df09616cd37214f9d
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-03 11:36:06 +0000

    Corrigido login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,28 +10,14 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
+      flash.now.alert = "Invalid email or password"
       redirect_to root_url, :alert => "Invalid email or password"
-    end
-
-    respond_to do |format|
-      format.html { redirect_to(root_url, :notice => 'Logged in.') }
-      format.xml  { head :ok }
-      format.json  { head :ok }
     end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
-  end
-
-  def index
-  end
-
-  def edit
-  end
-
-  def update
   end
 
 end
