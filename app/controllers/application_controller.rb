94a1384b07f3caa028acbc89f575681f9e39cfb5
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-03 11:42:25 +0000

    Experiencia com xml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,10 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    respond_to do |format|
+      format.html {redirect_to( root_url, :alert => exception.message) }
+      format.xml  { render :xml => { :error => 'Not found' } }
+    end
   end
   
 end
