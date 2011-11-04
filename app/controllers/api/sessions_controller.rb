ce49f0652e840800e6625748b00346369df03fa1
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-04 10:40:21 +0000

    ...

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -24,16 +24,6 @@ class Api::SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
-  end
-
-  def edit
-  end
-  
-  def update
-  end
-
-  def index
-  end
 
   skip_before_filter :verify_authenticity_token
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,4 +25,7 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
   end
+
+  
+  skip_before_filter :verify_authenticity_token
 end
