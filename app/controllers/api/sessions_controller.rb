08811cbf21af8d8b036a0e0c306818fe80e598e1
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-04 11:50:51 +0000

    ...

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,18 +7,6 @@ class Api::SessionsController < ApiController
 
   def create
     user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      respond_to do |format|
-        format.html {redirect_to(root_url, :notice => "Logged in!")}
-        format.xml { render :xml => session }
-      end
-    else
-      respond_to do |format|
-        format.html {redirect_to(root_url, :notice => 'Invalid email or password')}
-        format.xml  { render :xml => { :error => 'User not autenticated' }, :status =>  149 }
-      end
-    end
   end
 
   def destroy
