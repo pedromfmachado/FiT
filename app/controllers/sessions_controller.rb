621a53f0bfc031c19f0fa4779f6cc4fb97f67235
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-04 11:00:00 +0000

    ...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,21 +9,12 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      respond_to do |format|
-        format.html {redirect_to(root_url, :notice => "Logged in!")}
-        format.xml { render :xml => session }
-      end
+      respond_with session
     else
-      respond_to do |format|
-        format.html {redirect_to(root_url, :notice => 'Invalid email or password')}
-        format.xml  { render :xml => { :error => 'User not autenticated' }, :status =>  149 }
-      end
+      :xml => { :error => 'User not autenticated' }, :status =>  149
     end
   end
+end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
-  end
   skip_before_filter :verify_authenticity_token
 end
