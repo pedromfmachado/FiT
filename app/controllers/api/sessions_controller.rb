883130256a954ea48f258a90b6f92e55f74ebd8d
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-04 11:43:54 +0000

    ...

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -25,5 +25,6 @@ class Api::SessionsController < ApiController
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
   end
+  
   skip_before_filter :verify_authenticity_token
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,5 @@
 class ApiController < ApplicationController
   
   respond_to :xml
-  
-  skip_before_filter :verify_authenticity_token
 end
 
