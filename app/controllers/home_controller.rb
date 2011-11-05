e66286dd22110f056c85ade25b28704f26c198b2
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-05 12:26:57 +0000

    Corrigido site mobile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
   layout "home_layout"
 
   def show
-    render :layout => "home_layout"
+    respond_to do |format|
+      format.html {render :layout => "home_layout"}
+      format.mobile {redirect_to new_session_path }
+    end
   end
 
 end
--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,1 +1,4 @@
-$.jQTouch({});
+$.jQTouch({
+  statusBar: 'black'
+
+});
