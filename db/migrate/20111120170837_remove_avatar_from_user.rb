065d3c29a24f75e91c20d1b6cebfb0b982ad90c5
Pedro Machado <machado.pedro@fe.up.pt>
2011-11-20 21:21:22 +0000

    .

--- db/migrate/20111120170837_remove_avatar_from_user.rb
+++ db/migrate/20111120170837_remove_avatar_from_user.rb
@@ -1,9 +1,9 @@
 class RemoveAvatarFromUser < ActiveRecord::Migration
   def change
-    remove_column :users, :avatar_file_name, :string
-    remove_column :users, :avatar_content_type, :string
-    remove_column :users, :avatar_file_size, :integer
-    remove_column :users, :avatar_updated_at, :datetime
+    remove_column :users, :avatar_file_name
+    remove_column :users, :avatar_content_type
+    remove_column :users, :avatar_file_size
+    remove_column :users, :avatar_updated_at
     add_column :users, :url_foto, :string
   end
 end
